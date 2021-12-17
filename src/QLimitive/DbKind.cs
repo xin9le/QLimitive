66cf75467afe4c51f5e4d692f584069cee0811b1
Takaaki Suzuki <xin9le@live.jp>
2021-12-18 01:59:12 +0900

    Add database kinds

---
+++ src/QLimitive/DbKind.cs
@@ -0,0 +1,34 @@
+﻿namespace QLimitive;
+
+
+
+/// <summary>
+/// Represents kind of database.
+/// </summary>
+public enum DbKind : byte
+{
+    /// <summary>
+    /// SQL Server
+    /// </summary>
+    SqlServer = 0,
+
+    /// <summary>
+    /// MySQL / Amazon Aurora / MariaDB
+    /// </summary>
+    MySql,
+
+    /// <summary>
+    /// SQLite
+    /// </summary>
+    Sqlite,
+
+    /// <summary>
+    /// PostgreSQL
+    /// </summary>
+    PostgreSql,
+
+    /// <summary>
+    /// Oracle
+    /// </summary>
+    Oracle,
+}
