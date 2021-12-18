da7f5a7b782dac5a65da6186e9253503ef5a81b1
Takaaki Suzuki <xin9le@live.jp>
2021-12-18 18:34:17 +0900

    Add mapping informations

---
+++ src/QLimitive/Mappings/ColumnMappingInfo.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+
+namespace QLimitive.Mappings;
+
+
+
+/// <summary>
+/// Provides column mapping information.
+/// </summary>
+internal sealed class ColumnMappingInfo
+{
+    #region Properties
+    /// <summary>
+    /// Gets the name of member.
+    /// </summary>
+    public string MemberName { get; }
+
+
+    /// <summary>
+    /// Gets the type of member.
+    /// </summary>
+    public Type MemberType { get; }
+
+
+    /// <summary>
+    /// Gets the name of column.
+    /// </summary>
+    public string ColumnName { get; }
+
+
+    /// <summary>
+    /// Gets the type name of column.
+    /// </summary>
+    public string? ColumnTypeName { get; }
+
+
+    /// <summary>
+    /// Gets the order of column.
+    /// </summary>
+    public int ColumnOrder { get; }
+
+
+    /// <summary>
+    /// Gets the default value.
+    /// </summary>
+    public object? DefaultValue { get; }
+
+
+    /// <summary>
+    /// Gets whether primary key.
+    /// </summary>
+    public bool IsPrimaryKey { get; }
+
+
+    /// <summary>
+    /// Gets whether allowing null.
+    /// </summary>
+    public bool IsNullable { get; }
+
+
+    /// <summary>
+    /// Gets whether column mapping.
+    /// </summary>
+    public bool IsMapped { get; }
+
+
+    /// <summary>
+    /// Gets whether auto increment numbering.
+    /// </summary>
+    public bool IsAutoIncrement { get; }
+    #endregion
+
+
+    #region Constructors
+    /// <summary>
+    /// Creates instance.
+    /// </summary>
+    public ColumnMappingInfo(PropertyInfo property, NullabilityInfo nullability, bool existsCompositePrimaryKey)
+        : this(property, property.PropertyType, nullability, existsCompositePrimaryKey)
+    { }
+
+
+    /// <summary>
+    /// Creates instance.
+    /// </summary>
+    public ColumnMappingInfo(FieldInfo field, NullabilityInfo nullability, bool existsCompositePrimaryKey)
+        : this(field, field.FieldType, nullability, existsCompositePrimaryKey)
+    { }
+
+
+    /// <summary>
+    /// Creates instance.
+    /// </summary>
+    private ColumnMappingInfo(MemberInfo member, Type memberType, NullabilityInfo nullability, bool existsCompositePrimaryKey)
+    {
+        var columnAttr = member.GetCustomAttributes<ColumnAttribute>(true).FirstOrDefault();
+        var defaultAttr = member.GetCustomAttributes<DefaultValueAttribute>(true).FirstOrDefault();
+        var dbGeneratedAttr = member.GetCustomAttributes<DatabaseGeneratedAttribute>(true).FirstOrDefault();
+
+        this.MemberName = member.Name;
+        this.MemberType = memberType;
+        this.ColumnName = columnAttr?.Name ?? member.Name;
+        this.ColumnTypeName = columnAttr?.TypeName;
+        this.ColumnOrder = columnAttr?.Order ?? -1;
+        this.DefaultValue = defaultAttr?.Value;
+        this.IsPrimaryKey = Attribute.IsDefined(member, typeof(KeyAttribute));
+        this.IsNullable = nullability.ReadState == NullabilityState.Nullable;
+        this.IsMapped = !Attribute.IsDefined(member, typeof(NotMappedAttribute));
+        this.IsAutoIncrement = isAutoIncrement(dbGeneratedAttr, memberType, this.IsPrimaryKey, existsCompositePrimaryKey);
+
+        #region Local Functions
+        static bool isAutoIncrement(DatabaseGeneratedAttribute? attr, Type memberType, bool isPrimaryKey, bool existsCompositePrimaryKey)
+        {
+            //--- Explicit determination by attributes
+            if (attr is not null)
+                return attr.DatabaseGeneratedOption == DatabaseGeneratedOption.Identity;
+
+            //--- Automatic determination
+            if (isPrimaryKey && !existsCompositePrimaryKey)
+                return isNumberType(memberType);
+
+            return false;
+        }
+
+
+        static bool isNumberType(Type type)
+            => Type.GetTypeCode(type)
+            is TypeCode.Byte
+            or TypeCode.SByte
+            or TypeCode.UInt16
+            or TypeCode.UInt32
+            or TypeCode.UInt64
+            or TypeCode.Int16
+            or TypeCode.Int32
+            or TypeCode.Int64;
+        #endregion
+    }
+    #endregion
+}
---
+++ src/QLimitive/Mappings/TableMappingInfo.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using QLimitive.Internals;
+
+namespace QLimitive.Mappings;
+
+
+
+/// <summary>
+/// Provides table mapping information.
+/// </summary>
+internal sealed class TableMappingInfo
+{
+    #region Properties
+    /// <summary>
+    /// Gets the type that is mapped to the table.
+    /// </summary>
+    public Type Type { get; private init; }
+
+
+    /// <summary>
+    /// Gets the schema name.
+    /// </summary>
+    public string? Schema { get; private init; }
+
+
+    /// <summary>
+    /// Gets the table name.
+    /// </summary>
+    public string Name { get; private init; }
+
+
+    /// <summary>
+    /// Gets the column mapping information.
+    /// </summary>
+    public ReadOnlyArray<ColumnMappingInfo> Columns { get; private init; }
+    #endregion
+
+
+    #region Constructors
+#pragma warning disable CS8618
+    /// <summary>
+    /// Creates instance.
+    /// </summary>
+    private TableMappingInfo()
+    { }
+#pragma warning restore CS8618
+    #endregion
+
+
+    #region Get
+    /// <summary>
+    /// Gets the table mapping information corresponding to the specified type.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static TableMappingInfo Get<T>()
+        => Cache<T>.Instance;
+    #endregion
+
+
+    #region Internal Cache
+    /// <summary>
+    /// Provides <see cref="TableMappingInfo"/> cache.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    private static class Cache<T>
+    {
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        public static TableMappingInfo Instance { get; }
+
+
+        /// <summary>
+        /// Static constructors
+        /// </summary>
+        static Cache()
+        {
+            var type = typeof(T);
+            var tableAttr = type.GetCustomAttributes<TableAttribute>(true).FirstOrDefault();
+            Instance = new()
+            {
+                Type = type,
+                Schema = tableAttr?.Schema,
+                Name = tableAttr?.Name ?? type.Name,
+                Columns = getColumns().ToReadOnlyArray(),
+            };
+
+            #region Local Functions
+            static IEnumerable<ColumnMappingInfo> getColumns()
+            {
+                var type = typeof(T);
+                var flags = BindingFlags.Instance | BindingFlags.Public;
+                var nullabilityContext = new NullabilityInfoContext();
+
+                //--- Check if exists composite primary key
+                var properties = type.GetProperties(flags);
+                var fields = type.GetFields(flags);
+                var primaryKeyCount
+                    = properties.Cast<MemberInfo>()
+                    .Concat(fields.Cast<MemberInfo>())
+                    .Count(static x => Attribute.IsDefined(x, typeof(KeyAttribute)));
+                var existsCompositePrimaryKey = primaryKeyCount >= 2;
+
+                //--- Properties
+                foreach (var property in properties)
+                {
+                    var nullability = nullabilityContext.Create(property);
+                    yield return new ColumnMappingInfo(property, nullability, existsCompositePrimaryKey);
+                }
+
+                //--- Fields
+                foreach (var field in fields)
+                {
+                    var nullability = nullabilityContext.Create(field);
+                    yield return new ColumnMappingInfo(field, nullability, existsCompositePrimaryKey);
+                }
+            }
+            #endregion
+        }
+    }
+    #endregion
+}
