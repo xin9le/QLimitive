a558906c4f6640765afe25839d956f86a0a4fafa
Takaaki Suzuki <xin9le@live.jp>
2021-12-18 22:54:59 +0900

    Add SQL statement and bind parameter pair

---
+++ src/QLimitive/Query.cs
@@ -0,0 +1,34 @@
+﻿namespace QLimitive;
+
+
+
+/// <summary>
+/// Represents a query statement and parameter pair.
+/// </summary>
+public readonly struct Query
+{
+    #region Properties
+    /// <summary>
+    /// Gets SQL statement.
+    /// </summary>
+    public string Statement { get; }
+
+
+    /// <summary>
+    /// Gets bind parameter collection.
+    /// </summary>
+    public BindParameter? BindParameter { get; }
+    #endregion
+
+
+    #region Constructors
+    /// <summary>
+    /// Creates instance.
+    /// </summary>
+    internal Query(string statement, BindParameter? bindParameter)
+    {
+        this.Statement = statement;
+        this.BindParameter = bindParameter;
+    }
+    #endregion
+}
