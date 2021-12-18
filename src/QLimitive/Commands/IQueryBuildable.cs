10ba616983fe6bc75c4d9c99dd2ff0dc915daf80
Takaaki Suzuki <xin9le@live.jp>
2021-12-18 23:45:15 +0900

    Add IQueryBuildable interface

---
+++ src/QLimitive/Commands/IQueryBuildable.cs
@@ -0,0 +1,18 @@
+﻿using Cysharp.Text;
+
+namespace QLimitive.Commands;
+
+
+
+/// <summary>
+/// Represents the SQL buildable.
+/// </summary>
+internal interface IQueryBuildable
+{
+    /// <summary>
+    /// Build query.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="parameters"></param>
+    void Build(in Utf16ValueStringBuilder builder, ref BindParameterCollection? parameters);
+}
