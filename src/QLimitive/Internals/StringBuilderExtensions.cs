89513a801baa442fec8b7f48a4ca1ef2967c5143
Takaaki Suzuki <xin9le@live.jp>
2021-12-18 23:44:58 +0900

    Add an extension method to append full table name

---
+++ src/QLimitive/Internals/StringBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Cysharp.Text;
+using QLimitive.Mappings;
+
+namespace QLimitive.Internals;
+
+
+
+/// <summary>
+/// Provides the extension methods for <see cref="Utf16ValueStringBuilder"/>.
+/// </summary>
+internal static class StringBuilderExtensions
+{
+    /// <summary>
+    /// Append full table name.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="builder"></param>
+    /// <param name="dialect"></param>
+    /// <remarks>- i.g.) [dbo].[TableName]</remarks>
+    public static void AppendTableName<T>(in this Utf16ValueStringBuilder builder, DbDialect dialect)
+    {
+        var table = TableMappingInfo.Get<T>();
+        var schema = table.Schema ?? dialect.DefaultSchema;
+        var bracket = dialect.KeywordBracket;
+        if (string.IsNullOrWhiteSpace(schema))
+        {
+            builder.Append(bracket.Begin);
+            builder.Append(table.Name);
+            builder.Append(bracket.End);
+        }
+        else
+        {
+            builder.Append(bracket.Begin);
+            builder.Append(schema);
+            builder.Append(bracket.End);
+            builder.Append('.');
+            builder.Append(bracket.Begin);
+            builder.Append(table.Name);
+            builder.Append(bracket.End);
+        }
+    }
+}
