ad8c1d127b128b2a316b427d581ff726f9c62a72
Takaaki Suzuki <xin9le@live.jp>
2022-06-13 01:14:05 +0900

    Add AsIs test case

--- src/UnitTests/SqlServer/Cases/ComplexQueryTest.cs
+++ src/UnitTests/SqlServer/Cases/ComplexQueryTest.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using Cysharp.Text;
 using FluentAssertions;
+using QLimitive.Mappings;
 using QLimitive.UnitTests.SqlServer.Models;
 using Xunit;
 
@@ -152,6 +154,67 @@ order by
             {
                 builder.Select();
                 builder.Where(static x => x.Id == 1 || x.LastName != "xin9le" && x.Age > 20);
+                builder.OrderBy(static x => x.Id);
+                builder.ThenByDescending(static x => x.Age);
+                return builder.Build();
+            }
+        }
+    }
+
+
+    [Fact]
+    public void SelectWhereAsIsOrderByThenBy()
+    {
+        var actual = createQuery(this.Dialect);
+        var expect =
+@"select
+    [Id] as [Id],
+    [姓] as [LastName],
+    [名] as [FirstName],
+    [Age] as [Age],
+    [Sex] as [Sex],
+    [HasChildren] as [HasChildren],
+    [CreatedAt] as [CreatedAt],
+    [UpdatedAt] as [ModifiedAt]
+from [dbo].[T_People]
+where
+    [Id] = @p1 or ([姓] <> @p2 and [Age] > @p3)
+    and [姓] like @term
+order by
+    [Id],
+    [Age] desc";
+        actual.Text.Should().Be(expect);
+        actual.Parameters.Should().NotBeNull();
+        actual.Parameters.Should().Contain("p1", 1);
+        actual.Parameters.Should().Contain("p2", "xin9le");
+        actual.Parameters.Should().Contain("p3", 20);
+        actual.Parameters.Should().Contain("term", "csharp");
+
+        static Query createQuery(DbDialect dialect)
+        {
+            using (var builder = new QueryBuilder<Person>(dialect))
+            {
+                builder.Select();
+                builder.Where(static x => x.Id == 1 || x.LastName != "xin9le" && x.Age > 20);
+                builder.AsIs(static (ref Utf16ValueStringBuilder stringBuilder, ref BindParameterCollection? bindParameters, DbDialect dialect) =>
+                {
+                    var term = "csharp";
+                    var table = TableMappingInfo.Get<Person>();
+                    var bracket = dialect.KeywordBracket;
+                    var column = table.ColumnByMemberName[nameof(Person.LastName)];
+
+                    stringBuilder.AppendLine();
+                    stringBuilder.Append("    and ");
+                    stringBuilder.Append(bracket.Begin);
+                    stringBuilder.Append(column.ColumnName);
+                    stringBuilder.Append(bracket.End);
+                    stringBuilder.Append(" like ");
+                    stringBuilder.Append(dialect.BindParameterPrefix);
+                    stringBuilder.Append(nameof(term));
+
+                    bindParameters ??= new();
+                    bindParameters.Add(nameof(term), term);
+                }, dialect);
                 builder.OrderBy(static x => x.Id);
                 builder.ThenByDescending(static x => x.Age);
                 return builder.Build();
