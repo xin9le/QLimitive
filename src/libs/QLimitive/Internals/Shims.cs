fe4a0818cc28413d9229b66edd735628bfe75522
Takaaki Suzuki <xin9le@live.jp>
2025-11-26 00:11:17 +0900

    Represents as shims

---
+++ src/libs/QLimitive/Internals/Shims.cs
@@ -0,0 +1,31 @@
+﻿#if !NET10_0_OR_GREATER
+using System.Runtime.CompilerServices;
+#endif
+
+namespace QLimitive.Internals;
+
+
+
+/// <summary>
+/// Provides compatibility shims for APIs that are not available in all target .NET versions.
+/// </summary>
+internal static class Shims
+{
+#if !NET10_0_OR_GREATER
+    extension(DefaultInterpolatedStringHandler @this)
+    {
+        /// <summary>
+        /// Clears the handler.
+        /// </summary>
+        public void Clear()
+        {
+            DefaultInterpolatedStringHandler_Clear(ref @this);
+
+            #region Local Functions
+            [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Clear")]
+            static extern void DefaultInterpolatedStringHandler_Clear(ref DefaultInterpolatedStringHandler handler);
+            #endregion
+        }
+    }
+#endif
+}
